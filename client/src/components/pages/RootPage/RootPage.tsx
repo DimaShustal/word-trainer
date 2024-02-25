33ecb8bcb603930073084b5922e47fa2fae88dc7
DimaShustal <dzmitry.shustal@gmail.com>
2024-02-25 15:45:44 +0100

    [graphql] add content to RootPage.tsx

--- client/src/components/pages/RootPage/RootPage.tsx
+++ client/src/components/pages/RootPage/RootPage.tsx
@@ -1,10 +1,22 @@
 import { observer } from 'mobx-react-lite';
+import { useAppContext } from '../../../contexts/AppContext';
+import Typography from '../../atoms/Typography';
 
 function RootPage() {
+  const { store } = useAppContext();
+
   return (
-    <div>
-      <h1>RootPage</h1>
-    </div>
+    <>
+      <Typography variant="h5" bMargin={20}>
+        Добро пожаловать в лчушее приложение для изучения иностранных фраз!
+      </Typography>
+      <Typography variant="paragraphLarge" bMargin={10}>
+        Тут должен быть мотивационный текс и 100500 причин почему мы лучшие, но пока так.
+      </Typography>
+      <Typography variant="paragraphLarge">
+        В общем {store.user.isLogged ? 'фразы сами себя не выучат' : 'кнопка "Войти" себя не нажмет'}, начинай.
+      </Typography>
+    </>
   );
 }
 
