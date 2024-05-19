3cfbdef79332891ec5e18cbcb90f755353b3f56d
DimaShustal <dzmitry.shustal@gmail.com>
2024-05-19 18:29:09 +0200

    [timeout] add requestTimeout to login

--- server/src/graphql/rootValue/index.ts
+++ server/src/graphql/rootValue/index.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import createUser from './handlers/createUser.js';
 import login from './handlers/login.js';
 import user from './handlers/user.js';
@@ -6,10 +8,19 @@ import userWords from './handlers/userWords.js';
 import addWordsFromTranslation from './handlers/addWordsFromTranslation.js';
 import updateWords from './handlers/updateWords.js';
 import removeWords from './handlers/removeWords.js';
+import { SECOND_IN_MILLISECONDS } from '../../constants/time.js';
+
+const requestTimeout = async () => {
+  await new Promise(resolve => setTimeout(resolve, SECOND_IN_MILLISECONDS));
+
+  return new Error('Try later');
+};
 
 const rootValue = {
   createUser,
-  login,
+  login: (...args) => {
+    return Promise.race([login(...args), requestTimeout()]);
+  },
   user,
   languages,
   userWords,
